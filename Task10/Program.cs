1fd3212203acbcd1a8bfebe0ec88199500970f28
[Ruliaga] <ruliaga@mail.ru>
2022-08-23 00:47:37 +0500

    Task10 done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число:");
+int Number=int.Parse(Console.ReadLine());
+
+
+
+void CreateArrayFromNumber (int x){
+
+int [] array = new int[3];
+
+array[0]=x/100;
+array[1]=(x-array[0]*100)/10;
+array[2]=x-array[0]*100-array[1]*10;
+
+Console.Write($"{x} -> {array[1]}");
+}
+CreateArrayFromNumber(Number);
+
+
