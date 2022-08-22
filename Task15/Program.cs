5287b49ba6c09b22873301fef37cec2ec157e314
[Ruliaga] <ruliaga@mail.ru>
2022-08-23 03:00:40 +0500

    Task15 done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int number=int.Parse(Console.ReadLine());
+
+if (number>0 && number<6){
+
+    Console.Write($"{number} -> это будний день");
+
+} else if (number>5 & number<8){
+
+        Console.Write($"{number} -> это выходной день");
+
+} else{
+
+        Console.Write("Вы ввели недопустимое значение. Введите число от 1 до 7");
+
+
+}
